ef1f7fecd5efce0b57b117f4b3364a3fd9a57398
Gabriel Ribeiro <gmourafiho94@gmail.com>
2024-03-14 17:56:26 -0300

    add env variables

--- src/env.js
+++ src/env.js
@@ -11,7 +11,7 @@ export const env = createEnv({
       .string()
       .url()
       .refine(
-        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+        (str) => !str.includes(process.env.)
         "You forgot to change the default URL"
       ),
     NODE_ENV: z
@@ -33,7 +33,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URL: process.env.DATABASE_AURA_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
